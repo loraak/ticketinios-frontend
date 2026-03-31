d609755fb87f05e4b8ae253314cca9d7daba7b1b
Karol Arvizu <lorakarvizu@gmail.com>
2026-03-30 20:22:57 -0600

    ay no sé

--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 
 export interface UsuarioAdmin {
     id: string;
@@ -16,15 +16,21 @@ export class UsuarioService {
     private base = 'http://localhost:8081/api/usuarios';
 
     listar(): Observable<UsuarioAdmin[]> {
-        return this.http.get<UsuarioAdmin[]>(this.base, { withCredentials: true });
+        return this.http.get<any>(this.base, { withCredentials: true }).pipe(
+            map((response: any) => response.data) 
+        );
     }
 
     crear(data: any): Observable<UsuarioAdmin> {
-        return this.http.post<UsuarioAdmin>(this.base, data, { withCredentials: true });
+        return this.http.post<any>(this.base, data, { withCredentials: true }).pipe(
+            map((response: any) => response.data[0])
+        );
     }
 
     editar(id: string, data: any): Observable<UsuarioAdmin> {
-        return this.http.put<UsuarioAdmin>(`${this.base}/${id}`, data, { withCredentials: true });
+        return this.http.put<any>(`${this.base}/${id}`, data, { withCredentials: true }).pipe(
+            map((response: any) => response.data[0])
+        );
     }
 
     darDeBaja(id: string): Observable<any> {
