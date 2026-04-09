413b40bf7e61b6d10c48164ceabe69f3e3e8d745
kvazquez <karolgoretty.arvizu@gmail.com>
2026-04-09 14:50:37 -0600

    ultimo commit en chamba

--- src/app/directives/has-permission.directive.ts
+++ src/app/directives/has-permission.directive.ts
@@ -4,9 +4,9 @@ import { PermissionsService } from '../services/permissions.service';
 @Directive({
     selector: '[ifHasPermission]',
     standalone: true
-    })
-    export class HasPermissionDirective implements OnInit {
-    @Input('ifHasPermission') permisos: string | string[] = '';
+})
+export class HasPermissionDirective implements OnInit {
+    @Input('ifHasPermission') config: string | string[] | { permisos: string[], grupoId: string } = '';
 
     constructor(
         private permissionsSvc: PermissionsService,
@@ -15,10 +15,25 @@ import { PermissionsService } from '../services/permissions.service';
     ) {}
 
     ngOnInit() {
-        const permisosArray = Array.isArray(this.permisos) ? this.permisos : [this.permisos];
-
-        if (this.permissionsSvc.hasAnyPermission(permisosArray)) {
-        this.viewContainer.createEmbeddedView(this.templateRef);
+        let permisosArray: string[];
+        let grupoId: string | null = null;
+    
+        if (typeof this.config === 'object' && !Array.isArray(this.config)) {
+            permisosArray = this.config.permisos;
+            grupoId = this.config.grupoId;
+        } else {
+            permisosArray = Array.isArray(this.config) ? this.config : [this.config];
+        }
+    
+        if (grupoId) {
+            this.permissionsSvc.hasAnyGroupPermission(grupoId, permisosArray)
+                .subscribe(tiene => {
+                    if (tiene) this.viewContainer.createEmbeddedView(this.templateRef);
+                });
+        } else {
+            if (this.permissionsSvc.hasAnyPermission(permisosArray)) {
+                this.viewContainer.createEmbeddedView(this.templateRef);
+            }
         }
     }
 }
--- src/app/services/permissions.service.ts
+++ src/app/services/permissions.service.ts
@@ -1,8 +1,11 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable, signal, inject } from '@angular/core';
+import { Observable, catchError, map, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class PermissionsService {
     private userPermissions = signal<string[]>([]);
+    private http = inject(HttpClient);
 
     setPermissions(perms: string[]) {
         this.userPermissions.set(perms);
@@ -14,5 +17,15 @@ export class PermissionsService {
 
     hasAnyPermission(perms: string[]): boolean {
         return perms.some(p => this.hasPermission(p));
+    }
+
+    hasAnyGroupPermission(grupoId: string, permisos: string[]): Observable<boolean> {
+        return this.http.get<any>(`http://localhost:3000/api/grupos/${grupoId}/permisos`).pipe(
+            map((res: any) => {
+                const permisosGrupo: string[] = res.data ?? [];
+                return permisos.some(p => permisosGrupo.includes(p));
+            }),
+            catchError(() => of(false))
+        );
     }
 }
