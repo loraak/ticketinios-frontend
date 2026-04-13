c579e6748eb629cbd0bb7bbe2b6c2bf76d7785b0
Karol Arvizu <lorakarvizu@gmail.com>
2026-04-12 19:33:32 -0600

    detalles para el deploy, cosas de dockr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'ticketinios-apigateway-production.up.railway.app'
+    apiUrl: 'https://ticketinios-apigateway-production.up.railway.app'
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'ticketinios-apigateway-production.up.railway.app'
+    apiUrl: 'https://ticketinios-apigateway-production.up.railway.app'
 }
